d9e907e6b2d5fc6be9ee559cafe03b4c0c8aefea
AsadullahGithub <96863072+AsadullahGithub@users.noreply.github.com>
2022-01-29 19:57:02 +0600

    Add files via upload

---
+++ one.js
@@ -0,0 +1,15 @@
+function click1(){
+  document.getElementById('pic').src='a.jpg'
+}
+function click2(){
+  document.getElementById('pic').src='b.jpg'
+}
+function click3(){
+  document.getElementById('pic').src='c.jpg'
+}
+function click4(){
+  document.getElementById('pic').src='b.jpg'
+}
+function click5(){
+  document.getElementById('pic').src='e.jpg'
+}
